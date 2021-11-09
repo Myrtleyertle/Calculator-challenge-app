0e085d7c0d15574609c6b4fc4357c9fc0e88d356
Myrtleyertle <89166514+Myrtleyertle@users.noreply.github.com>
2021-11-09 04:16:37 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const numberButton = document.querySelectorAll('[data-number-button]');
+const operatorButton = document.querySelectorAll('[data-operator-button]');
+const clearButton = document.querySelector('[data-clear-button]');
+const equalsButton = document.querySelector('[data-equals-button]');
+const display = document.getElementById('display-value');
+const displayHistory = document.getElementById('display-history');
+const deleteButton = document.querySelector('[data-delete-button]');
+
+function deleteLast() {
+    let displayValue = display.textContent;
+    display.textContent = displayValue.substring(0, displayValue.length - 1);
+}
+deleteButton.addEventListener('click', deleteLast);
+
+function clearDisplay() {
+    display.textContent = "";
+}
+
+clearButton.addEventListener('click', clearDisplay);
+
+numberButton.forEach(button => {
+    button.addEventListener('click', () => {
+        display.textContent = display.textContent + button.textContent;
+    });
+});
+
+operatorButton.forEach(button => {
+    button.addEventListener('click', () => {
+        display.textContent = display.textContent + button.textContent;
+    });
+});
+
+equalsButton.addEventListener('click', () => {
+    display.textContent = eval(display.textContent);
+});
+
+displayHistory.textContent = display.textContent;
